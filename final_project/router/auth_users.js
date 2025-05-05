732f5525c4253b6b23562cdba2b7370b56112bfa
gerarddevries <gerard.devries1968@gmail.com>
2025-05-05 09:22:07 -0400

    Task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -51,8 +51,16 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const reviewText = req.query.review;
+    const userName = req.session.authorization['username'];
+
+    let book = books[isbn];
+    let reviews = book['reviews'];
+    reviews[userName] = reviewText;
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({message: "Review has been stored for isbn: " + isbn, review: reviewText, user: userName});
 });
 
 module.exports.authenticated = regd_users;
