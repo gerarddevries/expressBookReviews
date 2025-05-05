c1287527fb2661bb26c30ebdb0bb8a2a2c6947c4
gerarddevries <gerard.devries1968@gmail.com>
2025-05-05 06:57:33 -0400

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,9 +18,10 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    // Retrieve the email parameter from the request URL and send the corresponding friend's details
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
