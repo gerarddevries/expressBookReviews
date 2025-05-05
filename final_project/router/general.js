820ca1122170908cf72288e8b5070dfc845fbfc9
gerarddevries <gerard.devries1968@gmail.com>
2025-05-05 06:36:26 -0400

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,8 +2,8 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
-const public_users = express.Router();
 
+const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Send JSON response with formatted books data
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
