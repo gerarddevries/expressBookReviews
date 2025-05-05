74df064fb34babe75c1dfc110bba235bd68a6694
gerarddevries <gerard.devries1968@gmail.com>
2025-05-05 07:14:42 -0400

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,14 +25,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    // Filter the books array for any book with specified author
+    const foundBooks = Object.entries(books)
+    .filter(([_, book]) => book.author === author);
+    res.send(foundBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    // Filter the books array for any book with specified title
+    const foundBooks = Object.entries(books)
+    .filter(([_, book]) => book.title === title);
+    res.send(foundBooks);
 });
 
 //  Get book review
